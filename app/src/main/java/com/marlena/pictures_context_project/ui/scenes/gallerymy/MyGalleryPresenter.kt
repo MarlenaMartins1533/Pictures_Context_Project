13293fe85db240971e08b003790e4d73584cb5e4
MarlenaMartins1533 <marlenamotamartins@gmail.com>
2019-12-11 15:52:47 -0300

    Flag add

--- app/src/main/java/com/marlena/pictures_context_project/ui/scenes/gallerymy/MyGalleryPresenter.kt
+++ app/src/main/java/com/marlena/pictures_context_project/ui/scenes/gallerymy/MyGalleryPresenter.kt
@@ -4,16 +4,16 @@ import com.marlena.pictures_context_project.ui.model.ThePicture
 
 class MyGalleryPresenter(private val view: MyGallery.View) : MyGallery.Presenter {
 
-    lateinit var pictureList: ArrayList<ThePicture>
+    lateinit var mypicturesList: ArrayList<ThePicture>
 
     override fun getAllList() {
 
-        pictureList = arrayListOf( //temporário, tirar!
+        mypicturesList = arrayListOf( //temporário, tirar!
             ThePicture(
                 url = "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3b/Flat_plus_icon.svg/1024px-Flat_plus_icon.svg.png",
                 name = "Empty!"
             )
         )
-        view.setAllList(pictureList)
+        view.setAllList(mypicturesList)
     }
 }
--- app/src/main/java/com/marlena/pictures_context_project/ui/scenes/picture/PicturePresenter.kt
+++ app/src/main/java/com/marlena/pictures_context_project/ui/scenes/picture/PicturePresenter.kt
@@ -1,3 +1,17 @@
 package com.marlena.pictures_context_project.ui.scenes.picture
 
-class PicturePresenter(private val view: Picture.View) : Picture.Presenter {}
+import com.marlena.pictures_context_project.ui.model.MyPicturesRoom
+
+class PicturePresenter(private val view: Picture.View) : Picture.Presenter {
+    override fun insertMyPicture(myPicture: MyPicturesRoom) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun deleteMyPicture(myPicture: MyPicturesRoom) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun requestMessage(myPicture: MyPicturesRoom) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
